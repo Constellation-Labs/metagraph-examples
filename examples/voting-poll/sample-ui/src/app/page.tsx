f93f4ddd44777f7d605338f2f79890bd3f5c33fd
Juan David Suarez <juansuarezespitia@gmail.com>
2024-07-10 09:39:29 -0500

    Added: base card samples

--- examples/voting-poll/sample-ui/src/app/page.tsx
+++ examples/voting-poll/sample-ui/src/app/page.tsx
@@ -1,5 +1,18 @@
-import { PageFrame } from '../components';
+import { Card, PageFrame } from '../components';
 
 export default async function HomePage() {
-  return <PageFrame></PageFrame>;
+  return (
+    <PageFrame>
+      <Card>Sample Content</Card>
+      <Card variants={['padding-m']}>Sample Content</Card>
+      <Card variants={['padding-sm']}>Sample Content</Card>
+      <Card header={'Sample header'}>Sample Content</Card>
+      <Card header={'Sample header'} variants={['padding-m']}>
+        Sample Content
+      </Card>
+      <Card header={'Sample header'} variants={['padding-sm']}>
+        Sample Content
+      </Card>
+    </PageFrame>
+  );
 }
