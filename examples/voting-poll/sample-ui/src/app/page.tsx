94066f62539b9cb709dd853561fcf14f27710cf8
Juan David Suarez <juansuarezespitia@gmail.com>
2024-07-10 10:34:05 -0500

    Added: base input samples

--- examples/voting-poll/sample-ui/src/app/page.tsx
+++ examples/voting-poll/sample-ui/src/app/page.tsx
@@ -1,18 +1,63 @@
-import { Card, PageFrame } from '../components';
+'use client';
 
-export default async function HomePage() {
+import { useForm } from 'react-hook-form';
+
+import { Input, PageFrame } from '../components';
+import { buildRegisterField } from '../utils';
+
+export default function HomePage() {
+  const form = useForm();
+
+  const register = buildRegisterField(
+    form.register,
+    form.formState,
+    form.control
+  );
   return (
     <PageFrame>
-      <Card>Sample Content</Card>
-      <Card variants={['padding-m']}>Sample Content</Card>
-      <Card variants={['padding-sm']}>Sample Content</Card>
-      <Card header={'Sample header'}>Sample Content</Card>
-      <Card header={'Sample header'} variants={['padding-m']}>
-        Sample Content
-      </Card>
-      <Card header={'Sample header'} variants={['padding-sm']}>
-        Sample Content
-      </Card>
+      <Input label="label" {...register('a')} />
+      <Input label="label" description="decription" {...register('a')} />
+      <Input
+        label="label"
+        description="decription"
+        placeholder="placeholder"
+        {...register('a')}
+      />
+      <Input
+        type="select"
+        label="label"
+        options={[{ value: 'opt1' }, { value: 'opt2' }]}
+        {...register('b')}
+      />
+      <Input
+        type="select"
+        description="decription"
+        label="label"
+        options={[{ value: 'opt1' }, { value: 'opt2' }]}
+        {...register('b')}
+      />
+      <Input
+        type="select"
+        description="decription"
+        label="label"
+        placeholder="placeholder"
+        options={[{ value: 'opt1' }, { value: 'opt2' }]}
+        {...register('b')}
+      />
+      <Input type="add-options" label="label" {...register('c')} />
+      <Input
+        type="add-options"
+        label="label"
+        description="decription"
+        {...register('c')}
+      />
+      <Input
+        type="add-options"
+        label="label"
+        description="decription"
+        placeholder="placeholder"
+        {...register('c')}
+      />
     </PageFrame>
   );
 }
