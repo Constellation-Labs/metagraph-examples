635bed33661edd6bffb336b347304b9da6d7e0ee
Juan David Suarez <juansuarezespitia@gmail.com>
2024-07-11 11:05:12 -0500

    Added: base poll schema

---
+++ examples/voting-poll/sample-ui/src/schemas/poll.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod';
+
+export const PollSchema = z.object({
+  name: z
+    .string({ invalid_type_error: 'Invalid poll name' })
+    .min(1, 'Must contain at least 1 character(s)')
+    .max(128, { message: 'Must contain at most 128 character(s)' }),
+  owner: z
+    .string({ invalid_type_error: 'Invalid owner address' })
+    .min(1, 'Must contain at least 1 character(s)')
+    .max(128, { message: 'Must contain at most 128 character(s)' }),
+  options: z
+    .array(
+      z
+        .string({ invalid_type_error: 'Invalid poll option' })
+        .min(1, 'Must contain at least 1 character(s)')
+        .max(256, { message: 'Must contain at most 256 character(s)' }),
+      {
+        invalid_type_error: 'Invalid poll options'
+      }
+    )
+    .nonempty({ message: 'Invalid poll options, must specify at least one' }),
+  startSnapshotOrdinal: z.coerce.number({
+    invalid_type_error: 'Invalid poll start'
+  }),
+  endSnapshotOrdinal: z.coerce.number({
+    invalid_type_error: 'Invalid poll end'
+  })
+});
