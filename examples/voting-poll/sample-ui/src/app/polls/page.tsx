6966e896c04cb51d0b5bb7643ff3542f56a5fc37
Juan David Suarez <juansuarezespitia@gmail.com>
2024-07-12 19:40:40 -0500

    Added: support to fetch polls from the API

--- examples/voting-poll/sample-ui/src/app/polls/page.tsx
+++ examples/voting-poll/sample-ui/src/app/polls/page.tsx
@@ -1,79 +1,44 @@
-import { dag4 } from '@stardust-collective/dag4';
-
 import { PageFrame, PollCard } from '../../components';
+import { ButtonLink } from '../../components/Button/ButtonLink/component';
+import { MetagraphBaseURLs } from '../../consts';
+import { IPoll } from '../../types';
 
 import styles from './page.module.scss';
 
 export default async function HomePage() {
+  const response = await fetch(
+    MetagraphBaseURLs.metagraphL0 + '/data-application/polls',
+    { cache: 'no-store' }
+  );
+
+  if (response.status !== 200) {
+    throw new Error(
+      JSON.stringify(
+        {
+          errors: { serverErrors: [response.status, await response.text()] }
+        },
+        null,
+        2
+      )
+    );
+  }
+
+  const polls: [string, IPoll][] = await response.json();
+
   return (
     <PageFrame>
+      {polls.length === 0 && (
+        <section className={styles.noPolls}>
+          <span>No polls yet, create one!</span>
+          <ButtonLink variants={['primary']} href={'/polls/create'}>
+            Create Poll
+          </ButtonLink>
+        </section>
+      )}
       <section className={styles.main}>
-        <PollCard
-          poll={{
-            id: 'hash1',
-            name: '$poll_name$',
-            owner: dag4.keyStore.getDagAddressFromPrivateKey(
-              dag4.keyStore.generatePrivateKey()
-            ),
-            pollOptions: ['Opt1', 'Opt2', 'Opt3', 'Opt4'],
-            startSnapshotOrdinal: 1129321,
-            endSnapshotOrdinal: 1229321,
-            results: [
-              { option: 'Opt1', votes: 1281 },
-              { option: 'Opt2', votes: 4352 },
-              { option: 'Opt3', votes: 1733 },
-              { option: 'Opt4', votes: 2957 }
-            ]
-          }}
-        />
-        <PollCard
-          poll={{
-            id: 'hash2',
-            name: '$poll_name$',
-            owner: dag4.keyStore.getDagAddressFromPrivateKey(
-              dag4.keyStore.generatePrivateKey()
-            ),
-            pollOptions: ['Opt1', 'Opt2', 'Opt3', 'Opt4'],
-            startSnapshotOrdinal: 1129321,
-            endSnapshotOrdinal: 1229321
-          }}
-        />
-        <PollCard
-          poll={{
-            id: 'hash3',
-            name: '$poll_name$',
-            owner: dag4.keyStore.getDagAddressFromPrivateKey(
-              dag4.keyStore.generatePrivateKey()
-            ),
-            pollOptions: ['Opt1', 'Opt2', 'Opt3', 'Opt4'],
-            startSnapshotOrdinal: 1129321,
-            endSnapshotOrdinal: 1229321
-          }}
-        />
-        <PollCard
-          poll={{
-            id: 'hash4',
-            name: '$poll_name$',
-            owner: dag4.keyStore.getDagAddressFromPrivateKey(
-              dag4.keyStore.generatePrivateKey()
-            ),
-            pollOptions: ['Opt1', 'Opt2', 'Opt3', 'Opt4'],
-            startSnapshotOrdinal: 1129321,
-            endSnapshotOrdinal: 1229321
-          }}
-        />
-        <PollCard
-          poll={{
-            id: 'hash5',
-            name: '$poll_name$',
-            owner: dag4.keyStore.getDagAddressFromPrivateKey(
-              dag4.keyStore.generatePrivateKey()
-            ),
-            pollOptions: ['Opt1', 'Opt2', 'Opt3', 'Opt4'],
-            startSnapshotOrdinal: 1129321,
-            endSnapshotOrdinal: 1229321
-          }}
-        />
+        {polls.map(([id, poll]) => (
+          <PollCard key={id} poll={poll} />
+        ))}
       </section>
     </PageFrame>
   );
