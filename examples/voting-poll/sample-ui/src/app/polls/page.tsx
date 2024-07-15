32d6e845b241f7f1cb27a33542fadf154d142718
Juan David Suarez <juansuarezespitia@gmail.com>
2024-07-15 17:02:46 -0500

    Added: explainer text / top

--- examples/voting-poll/sample-ui/src/app/polls/page.tsx
+++ examples/voting-poll/sample-ui/src/app/polls/page.tsx
@@ -1,4 +1,5 @@
-import { PageFrame, PollCard } from '../../components';
+/* eslint-disable react/no-unescaped-entities */
+import { Card, PageFrame, PollCard } from '../../components';
 import { ButtonLink } from '../../components/Button/ButtonLink/component';
 import { MetagraphBaseURLs } from '../../consts';
 import { IPoll } from '../../types';
@@ -33,11 +34,48 @@ export default async function HomePage() {
 
   return (
     <PageFrame variants={['noSidebarMargin']}>
-      <section className={styles.noPolls}>
-        {polls.length === 0 && <span>No polls yet, create one!</span>}
-        <ButtonLink variants={['primary']} href={'/polls/create'}>
-          Create Poll
-        </ButtonLink>
+      <section className={styles.introCard}>
+        <Card
+          header="Welcome to Metagraph Voting Poll Example"
+          variants={['padding-m']}
+        >
+          <p>
+            This application leverages Constellation's Hypergraph Transfer
+            Protocol (HGTP) to facilitate the creation and management of
+            decentralized polls and votes. By connecting to a custom metagraph
+            and interacting with the Stargazer Wallet, the app securely signs
+            and submits payloads to the network.
+            <br />
+            <br />
+            Metagraph state can be queried through the following links:
+            <ul>
+              <li>
+                <a
+                  href="http://localhost:9200/snapshots/latest/combined"
+                  target="_blank"
+                >
+                  http://localhost:9200/snapshots/latest/combined
+                </a>
+                : Access the latest snapshot data.{' '}
+              </li>
+              <li>
+                <a
+                  href="http://localhost:9200/data-application/polls"
+                  target="_blank"
+                >
+                  http://localhost:9200/data-application/polls
+                </a>
+                : Monitor the current state of polls.
+              </li>
+            </ul>
+          </p>
+          <div className={styles.createPolls}>
+            {polls.length === 0 && <span>No polls yet, create one!</span>}
+            <ButtonLink variants={['primary']} href={'/polls/create'}>
+              Create Poll
+            </ButtonLink>
+          </div>
+        </Card>
       </section>
       <section className={styles.main}>
         {polls.map(([id, poll]) => (
