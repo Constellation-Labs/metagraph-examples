a05a211eb6a406c9cf19a5cf971bc5be94f15241
Marcus Vinicius Girolneto Sousa <marcus.girolneto@gmail.com>
2024-09-30 15:17:16 -0300

    feat: adding nft-fee-transaction example

---
+++ examples/nft-fee-transaction/project/Dependencies.scala
@@ -0,0 +1,44 @@
+import sbt.*
+
+object Dependencies {
+
+  object V {
+    val tessellation = "99.99.99" //TODO: Update with the official version with FeeTransactions
+    val decline = "2.4.1"
+  }
+
+  def tessellation(artifact: String): ModuleID = "io.constellationnetwork" %% s"tessellation-$artifact" % V.tessellation
+
+  def decline(artifact: String = ""): ModuleID =
+    "com.monovore" %% {
+      if (artifact.isEmpty) "decline" else s"decline-$artifact"
+    } % V.decline
+
+  object Libraries {
+    val tessellationNodeShared = tessellation("node-shared")
+    val tessellationCurrencyL0 = tessellation("currency-l0")
+    val tessellationCurrencyL1 = tessellation("currency-l1")
+    val declineCore = decline()
+    val declineEffect = decline("effect")
+    val declineRefined = decline("refined")
+  }
+
+
+  // Scalafix rules
+  val organizeImports = "com.github.liancheng" %% "organize-imports" % "0.5.0"
+
+  object CompilerPlugin {
+
+    val betterMonadicFor = compilerPlugin(
+      "com.olegpy" %% "better-monadic-for" % "0.3.1"
+    )
+
+    val kindProjector = compilerPlugin(
+      ("org.typelevel" % "kind-projector" % "0.13.2").cross(CrossVersion.full)
+    )
+
+    val semanticDB = compilerPlugin(
+      ("org.scalameta" % "semanticdb-scalac" % "4.7.1").cross(CrossVersion.full)
+    )
+  }
+}
