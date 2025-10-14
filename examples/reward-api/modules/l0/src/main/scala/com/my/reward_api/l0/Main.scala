f510e071fbbdc42a7336536618f2e10fea5a9b04
Nick Meessen - de Wit <2356633+nickmeessen@users.noreply.github.com>
2025-10-14 12:27:34 +0700

    update custom rewards example

--- examples/reward-api/modules/l0/src/main/scala/com/my/reward_api/l0/Main.scala
+++ examples/reward-api/modules/l0/src/main/scala/com/my/reward_api/l0/Main.scala
@@ -9,20 +9,20 @@ import eu.timepit.refined.numeric.Positive
 import eu.timepit.refined.refineV
 import eu.timepit.refined.types.numeric.PosLong
 import io.circe.parser.decode
-import org.tessellation.BuildInfo
-import org.tessellation.currency.dataApplication.DataCalculatedState
-import org.tessellation.currency.l0.CurrencyL0App
-import org.tessellation.currency.schema.currency.{CurrencyIncrementalSnapshot, CurrencySnapshotStateProof}
-import org.tessellation.node.shared.domain.rewards.Rewards
-import org.tessellation.node.shared.infrastructure.consensus.trigger.ConsensusTrigger
-import org.tessellation.node.shared.snapshot.currency.CurrencySnapshotEvent
-import org.tessellation.schema.address.Address
-import org.tessellation.schema.balance.Balance
-import org.tessellation.schema.cluster.ClusterId
-import org.tessellation.schema.semver.{MetagraphVersion, TessellationVersion}
-import org.tessellation.schema.transaction.{RewardTransaction, Transaction, TransactionAmount}
-import org.tessellation.security.SecurityProvider
-import org.tessellation.security.signature.Signed
+import io.constellationnetwork.BuildInfo
+import io.constellationnetwork.currency.dataApplication.DataCalculatedState
+import io.constellationnetwork.currency.l0.CurrencyL0App
+import io.constellationnetwork.currency.schema.currency.{CurrencyIncrementalSnapshot, CurrencySnapshotStateProof}
+import io.constellationnetwork.node.shared.domain.rewards.Rewards
+import io.constellationnetwork.node.shared.infrastructure.consensus.trigger.ConsensusTrigger
+import io.constellationnetwork.node.shared.snapshot.currency.CurrencySnapshotEvent
+import io.constellationnetwork.schema.address.Address
+import io.constellationnetwork.schema.balance.Balance
+import io.constellationnetwork.schema.cluster.ClusterId
+import io.constellationnetwork.schema.semver.{MetagraphVersion, TessellationVersion}
+import io.constellationnetwork.schema.transaction.{RewardTransaction, Transaction, TransactionAmount}
+import io.constellationnetwork.security.SecurityProvider
+import io.constellationnetwork.security.signature.Signed
 
 import java.util.UUID
 import scala.collection.immutable.{SortedMap, SortedSet}
